84b7b106e9cd76377351a704eef8559db31ea480
Mark Servilla <servilla@lternet.edu>
2013-11-19 10:50:08 -0700

    create_schemas.sql
    Add time zone support for timestamps.

--- IdentityManager/WebRoot/WEB-INF/conf/create_schemas.sql
+++ IdentityManager/WebRoot/WEB-INF/conf/create_schemas.sql
@@ -15,7 +15,7 @@ CREATE SEQUENCE identity.profile_id_seq;
 CREATE TABLE identity.profile (
   profile_id INTEGER DEFAULT NEXTVAL('identity.profile_id_seq'),
   active BOOLEAN NOT NULL,
-  create_date TIMESTAMP NOT NULL,
+  create_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
   sur_name TEXT,
   given_name TEXT,
   nick_name TEXT,
@@ -29,7 +29,7 @@ CREATE TABLE identity.identity (
   user_id TEXT NOT NULL,
 	provider_id INTEGER NOT NULL,
 	profile_id INTEGER NOT NULL,
-	verification_date TIMESTAMP NOT NULL,
+	verification_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
   PRIMARY KEY (user_id, provider_id),
   FOREIGN KEY (profile_id) REFERENCES identity.profile(profile_id),
   FOREIGN KEY (provider_id) REFERENCES identity.provider(provider_id)
