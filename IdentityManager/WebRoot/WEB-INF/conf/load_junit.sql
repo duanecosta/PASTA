b6893b5d8f1d3b1418f914dad7fc2a21e9b2823e
Mark Servilla <servilla@lternet.edu>
2013-11-19 20:59:51 -0700

    load_junit.sql
    Add new user "cjack" and provider "LTERX".

--- IdentityManager/WebRoot/WEB-INF/conf/load_junit.sql
+++ IdentityManager/WebRoot/WEB-INF/conf/load_junit.sql
@@ -1,12 +1,14 @@
 INSERT INTO identity.profile
   (active,create_timestamp,update_timestamp,sur_name,given_name,nick_name,institution,email,intent)
 VALUES
-  (true, '2013-11-19 13:40:13-7','2013-11-19 13:40:13-7','Carroll','Utah','Dusty','LTER','ucarroll@lternet.edu','Research and testing');
+  (true, '2013-11-19 13:40:13-7','2013-11-19 13:40:13-7','Carroll','Utah','Dusty','LTER','ucarroll@lternet.edu','Research and testing'),
+  (true, '2013-11-19 18:10:24-7','2013-11-19 18:10:24-7','Jack','Cactus','Prickly','LTER','cjack@lternet.edu','Research and development');
 
 INSERT INTO identity.provider
   (provider_name,provider_conn,contact_name,contact_phone,contact_email)
 VALUES
-  ('LTER','ldaps://ldap.lternet.edu:636','System Administrator','505-277-2551','tech-support@lternet.edu');
+  ('LTER','ldaps://ldap.lternet.edu:636','System Administrator','505-277-2551','tech-support@lternet.edu'),
+  ('LTERX','jdbc:postgresql://db.lternet.edu/user','System Administrator','505-277-2551','tech-support@lternet.edu');
 
 INSERT INTO identity.identity
   (user_id,provider_id,profile_id,verify_timestamp)
