3c242efe7a9df1581b941ab92552f41230fe9256
Mark Servilla <servilla@lternet.edu>
2013-12-23 13:42:53 -0700

    Profile.java
    Add constructor with db load; refactor timestamp methods to use java.util.Date;
    add setter/getter for groups; add db getProfile/saveProfile/updateProfile methods.

--- IdentityManager/src/edu/lternet/pasta/identitymanager/Profile.java
+++ IdentityManager/src/edu/lternet/pasta/identitymanager/Profile.java
@@ -22,7 +22,8 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.log4j.Logger;
 
 import java.sql.*;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.Date;
 
 /**
  * User: servilla
@@ -39,7 +40,7 @@ public class Profile {
   /* Instance variables */
 
   private Integer profileId;
-  private boolean active;
+  private Boolean active;
   private Timestamp createTimestamp;
   private Timestamp updateTimestamp;
   private String surName;
@@ -62,17 +63,92 @@ public class Profile {
 
   /* Constructors */
 
+  /**
+   * Create a new Profile.
+   *
+   * @throws PastaConfigurationException
+   */
   public Profile() throws PastaConfigurationException {
 
     loadConfiguration();
 
   }
 
-  public Profile(Integer profileId) throws PastaConfigurationException {
+  /**
+   * Creates a new Profile from the Profile record in the Profile database based
+   * on the profile identifier.
+   *
+   * @param profileId Profile identifier
+   * @throws PastaConfigurationException
+   * @throws ProfileDoesNotExistException
+   * @throws SQLException
+   * @throws ClassNotFoundException
+   */
+  public Profile(Integer profileId) throws PastaConfigurationException,
+                                               ProfileDoesNotExistException,
+                                               SQLException,
+                                               ClassNotFoundException {
+
+    this.profileId = profileId;
 
     loadConfiguration();
 
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("SELECT identity.profile.active,");
+    strBuilder.append("identity.profile.create_timestamp,");
+    strBuilder.append("identity.profile.update_timestamp,");
+    strBuilder.append("identity.profile.sur_name,");
+    strBuilder.append("identity.profile.given_name,");
+    strBuilder.append("identity.profile.nick_name,");
+    strBuilder.append("identity.profile.institution,");
+    strBuilder.append("identity.profile.email,");
+    strBuilder.append("identity.profile.intent FROM identity.profile WHERE ");
+    strBuilder.append("profile_id=");
+    strBuilder.append(this.profileId);
+    strBuilder.append(";");
 
+    String sql = strBuilder.toString();
+
+    Connection dbConn;
+
+    try {
+      dbConn = getConnection();
+    }
+    catch (ClassNotFoundException e) {
+      logger.error("Profile: " + e);
+      e.printStackTrace();
+      throw e;
+    }
+
+    try {
+      Statement stmt = dbConn.createStatement();
+      ResultSet rs = stmt.executeQuery(sql);
+
+      if (rs.next()) {
+        active = rs.getBoolean("active");
+        createTimestamp = rs.getTimestamp("create_timestamp");
+        updateTimestamp = rs.getTimestamp("update_timestamp");
+        surName = rs.getString("sur_name");
+        givenName = rs.getString("given_name");
+        nickName = rs.getString("nick_name");
+        institution = rs.getString("institution");
+        email = rs.getString("email");
+        intent = rs.getString("intent");
+      } else {
+        String gripe = "Profile does not exist for profileId '" + profileId +
+                        "'!\n";
+        throw new ProfileDoesNotExistException(gripe);
+      }
+    }
+    catch (SQLException e) {
+      logger.error("Profile: " + e);
+      logger.error(sql);
+      e.printStackTrace();
+      throw e;
+    }
+    finally {
+      dbConn.close();
+    }
 
   }
 
@@ -117,37 +193,57 @@ public class Profile {
   /**
    * Gets the create timestamp of the Profile.
    *
-   * @return Create timestamp of the Profoile
+   * @return Create timestamp of the Profile
    */
-  public Timestamp getCreateTimestamp() {
-    return createTimestamp;
+  public Date getCreateTimestamp() {
+
+    Date date = null;
+
+    if (this.createTimestamp != null)
+      date = new Date(this.createTimestamp.getTime());
+
+    return date;
+
   }
 
   /**
    * Set the create timestamp of the Profile.
    *
-   * @param createTimestamp
+   * @param date Profile create timestamp
    */
-  public void setCreateTimestamp(Timestamp createTimestamp) {
-    this.createTimestamp = createTimestamp;
+  public void setCreateTimestamp(Date date) {
+
+    if (date != null)
+      this.createTimestamp = new Timestamp(date.getTime());
+
   }
 
   /**
    * Gets the update timestamp of the Profile.
    *
-   * @return Update timestamp of the Profile.
+   * @return Update timestamp of the Profile
    */
-  public Timestamp getUpdateTimestamp() {
-    return updateTimestamp;
+  public Date getUpdateTimestamp() {
+
+    Date date = null;
+
+    if (this.updateTimestamp != null)
+      date = new Date(this.updateTimestamp.getTime());
+
+    return date;
+
   }
 
   /**
    * Sets the update timestamp of the Profile.
    *
-   * @param updateTimestamp
+   * @param date Profile update timestamp
    */
-  public void setUpdateTimestamp(Timestamp updateTimestamp) {
-    this.updateTimestamp = updateTimestamp;
+  public void setUpdateTimestamp(Date date) {
+
+    if (date != null)
+      this.updateTimestamp = new Timestamp(date.getTime());
+
   }
 
   /**
@@ -259,6 +355,24 @@ public class Profile {
   }
 
   /**
+   * Gets the list of user groups associated with the Profile.
+   *
+   * @return List of user groups
+   */
+  public ArrayList<Group> getGroups() {
+    return groups;
+  }
+
+  /**
+   * Sets the list of user groups associated with the Profile.
+   *
+   * @param groups
+   */
+  public void setGroups(ArrayList<Group> groups) {
+    this.groups = groups;
+  }
+
+  /**
    * Gets the list of identities associated with the Profile.
    *
    * @return List of identities
@@ -328,21 +442,285 @@ public class Profile {
   }
 
   /**
-   * Gets the list of user groups associated with the Profile.
+   * Loads a Profile from the Profile record in the Profile database based
+   * on the profile identifier.
    *
-   * @return List of user groups
+   * @param profileId Profile identifier
+   * @throws PastaConfigurationException
+   * @throws ProfileDoesNotExistException
+   * @throws SQLException
+   * @throws ClassNotFoundException
    */
-  public ArrayList<Group> getGroups() {
-    return groups;
+  public void getProfile(Integer profileId) throws PastaConfigurationException,
+                                                       ProfileDoesNotExistException,
+                                                       SQLException,
+                                                       ClassNotFoundException {
+
+    this.profileId = profileId;
+
+    loadConfiguration();
+
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("SELECT identity.profile.active,");
+    strBuilder.append("identity.profile.create_timestamp,");
+    strBuilder.append("identity.profile.update_timestamp,");
+    strBuilder.append("identity.profile.sur_name,");
+    strBuilder.append("identity.profile.given_name,");
+    strBuilder.append("identity.profile.nick_name,");
+    strBuilder.append("identity.profile.institution,");
+    strBuilder.append("identity.profile.email,");
+    strBuilder.append("identity.profile.intent FROM identity.profile WHERE ");
+    strBuilder.append("profile_id=");
+    strBuilder.append(this.profileId);
+    strBuilder.append(";");
+
+    String sql = strBuilder.toString();
+
+    Connection dbConn;
+
+    try {
+      dbConn = getConnection();
+    }
+    catch (ClassNotFoundException e) {
+      logger.error("getProfile: " + e);
+      e.printStackTrace();
+      throw e;
+    }
+
+    try {
+      Statement stmt = dbConn.createStatement();
+      ResultSet rs = stmt.executeQuery(sql);
+
+      if (rs.next()) {
+        active = rs.getBoolean("active");
+        createTimestamp = rs.getTimestamp("create_timestamp");
+        updateTimestamp = rs.getTimestamp("update_timestamp");
+        surName = rs.getString("sur_name");
+        givenName = rs.getString("given_name");
+        nickName = rs.getString("nick_name");
+        institution = rs.getString("institution");
+        email = rs.getString("email");
+        intent = rs.getString("intent");
+      } else {
+        String gripe = "Profile does not exist for profileId '" + profileId +
+                           "'!\n";
+        throw new ProfileDoesNotExistException(gripe);
+      }
+    }
+    catch (SQLException e) {
+      logger.error("getProfile: " + e);
+      logger.error(sql);
+      e.printStackTrace();
+      throw e;
+    }
+    finally {
+      dbConn.close();
+    }
+
   }
 
   /**
-   * Sets the list of user groups associated with the Profile.
+   * Saves a Profile to the Profile database.
    *
-   * @param groups
+   * @throws ClassNotFoundException
+   * @throws SQLException
    */
-  public void setGroups(ArrayList<Group> groups) {
-    this.groups = groups;
+  public void saveProfile() throws ClassNotFoundException, SQLException {
+
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("INSERT INTO identity.profile ");
+    strBuilder.append("(active,create_timestamp,update_timestamp,sur_name,");
+    strBuilder.append("given_name,nick_name,institution,email,intent) ");
+    strBuilder.append("VALUES (");
+
+    if (active) {
+      strBuilder.append("TRUE,'");
+    }
+    else {
+      strBuilder.append("FALSE,'");
+    }
+
+    strBuilder.append(createTimestamp);
+    strBuilder.append("','");
+    strBuilder.append(updateTimestamp);
+    strBuilder.append("','");
+    strBuilder.append(surName);
+    strBuilder.append("','");
+    strBuilder.append(givenName);
+    strBuilder.append("','");
+    strBuilder.append(nickName);
+    strBuilder.append("','");
+    strBuilder.append(institution);
+    strBuilder.append("','");
+    strBuilder.append(email);
+    strBuilder.append("','");
+    strBuilder.append(intent);
+    strBuilder.append("');");
+
+    String sql = strBuilder.toString();
+
+    Connection dbConn;
+
+    try {
+      dbConn = getConnection();
+    }
+    catch (ClassNotFoundException e) {
+      logger.error("saveProfile: " + e);
+      e.printStackTrace();
+      throw e;
+    }
+
+    try {
+      Statement stmt = dbConn.createStatement();
+
+      if (stmt.executeUpdate(sql) == 0) {
+        String gripe = "saveProfile: '" + sql + "' failed";
+        throw new SQLException(gripe);
+      }
+
+    }
+    catch (SQLException e) {
+      logger.error("saveProfile: " + e);
+      logger.error(sql);
+      e.printStackTrace();
+      throw e;
+    }
+    finally {
+      dbConn.close();
+    }
+
+  }
+
+  /**
+   * Updates a Profile to a Profile record in the Profile database.
+   *
+   * @throws ClassNotFoundException
+   * @throws SQLException
+   */
+  public void updateProfile() throws ClassNotFoundException, SQLException {
+
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("UPDATE identity.profile SET ");
+    strBuilder.append("(active,create_timestamp,update_timestamp,sur_name,");
+    strBuilder.append("given_name,nick_name,institution,email,intent) ");
+    strBuilder.append("= (");
+
+    if (active) {
+      strBuilder.append("TRUE,'");
+    }
+    else {
+      strBuilder.append("FALSE,'");
+    }
+
+    strBuilder.append(createTimestamp);
+    strBuilder.append("','");
+    strBuilder.append(updateTimestamp);
+    strBuilder.append("','");
+    strBuilder.append(surName);
+    strBuilder.append("','");
+    strBuilder.append(givenName);
+    strBuilder.append("','");
+    strBuilder.append(nickName);
+    strBuilder.append("','");
+    strBuilder.append(institution);
+    strBuilder.append("','");
+    strBuilder.append(email);
+    strBuilder.append("','");
+    strBuilder.append(intent);
+    strBuilder.append("') WHERE identity.profile_id=");
+    strBuilder.append(profileId);
+    strBuilder.append(";");
+
+    String sql = strBuilder.toString();
+
+    Connection dbConn;
+
+    try {
+      dbConn = getConnection();
+    }
+    catch (ClassNotFoundException e) {
+      logger.error("updateProfile: " + e);
+      e.printStackTrace();
+      throw e;
+    }
+
+    try {
+      Statement stmt = dbConn.createStatement();
+
+      if (stmt.executeUpdate(sql) == 0) {
+        String gripe = "updateProfile: '" + sql + "' failed";
+        throw new SQLException(gripe);
+      }
+
+    }
+    catch (SQLException e) {
+      logger.error("updateProfile: " + e);
+      logger.error(sql);
+      e.printStackTrace();
+      throw e;
+    }
+    finally {
+      dbConn.close();
+    }
+
+  }
+
+  /**
+   * Deletes a Profile from Profile database.
+   *
+   * @throws ClassNotFoundException
+   * @throws SQLException
+   */
+  public void deleteProfile() throws ClassNotFoundException, SQLException {
+
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("DELETE FROM identity.profile WHERE profile_id=");
+    strBuilder.append(profileId);
+    strBuilder.append(";");
+
+    String sql = strBuilder.toString();
+
+    Connection dbConn;
+
+    try {
+      dbConn = getConnection();
+    }
+    catch (ClassNotFoundException e) {
+      logger.error("deleteProfile: " + e);
+      e.printStackTrace();
+      throw e;
+    }
+
+    try {
+      Statement stmt = dbConn.createStatement();
+
+      if (stmt.executeUpdate(sql) == 0) {
+        String gripe = "deleteProfile: '" + sql + "' failed";
+        throw new SQLException(gripe);
+      }
+
+    }
+    catch (SQLException e) {
+      logger.error("deleteProfile: " + e);
+      logger.error(sql);
+      e.printStackTrace();
+      throw e;
+    }
+    finally {
+      dbConn.close();
+    }
+
+    profileId = null;
+    active = null;
+    createTimestamp = null;
+    updateTimestamp = null;
+    surName = null;
+    givenName = null;
+    nickName = null;
+    institution = null;
+    email = null;
+    intent = null;
+
   }
 
   /*
@@ -415,13 +793,5 @@ public class Profile {
   }
 
   /* Class methods */
-
-  /*
-   * Sets the database URL to a new connection string (intended use is for unit
-   * testing).
-   */
-  protected static void setDatabase(String name) {
-    dbURL = "jdbc:postgresql://localhost/" + name;
-  }
 
 }
