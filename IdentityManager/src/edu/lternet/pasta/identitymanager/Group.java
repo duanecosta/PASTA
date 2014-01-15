e8bcf3f7287359852a29d8c34006b2053ae6e771
Mark Servilla <servilla@lternet.edu>
2014-01-15 14:59:59 -0700

    Group.java
    Remove from common.

--- IdentityManager/src/edu/lternet/pasta/identitymanager/Group.java
+++ IdentityManager/src/edu/lternet/pasta/identitymanager/Group.java
@@ -16,7 +16,7 @@
  * language governing permissions and limitations under the License.
  */
 
-package edu.lternet.pasta.common.security.authentication;
+package edu.lternet.pasta.identitymanager;
 
 /**
  * User: servilla
